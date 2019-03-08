423fce08dc23785853985b42e3216adaa45dddcd
[dedysimamora] <[dedysimamora627@gmail.com]>
2019-03-08 09:45:10 +0700

    upload tugas 3 dan 4

---
+++ Exercise3-iflese-javascript.js
@@ -0,0 +1,27 @@
+var nama = "junto";
+var peran = "dokter";
+
+if (nama === "" && peran === ""){
+
+  console.log("Nama harurs di isi!!");
+
+} else if (nama === "junto" && peran === "") {
+
+  console.log("Halo",nama, "Peran harus di isi");
+
+} else if (nama === "junto" && peran === "dukun") {
+
+  console.log("Selamat Datang Di dunia gaib " + nama );
+  console.log("Halo",peran ,nama, "Kamu dapat menyantet dengan matramu !" );
+
+} else if (nama === "junto" && peran === "dokter") {
+
+  console.log("Selamat Datang Di dunia gaib " + nama );
+  console.log("Halo",peran ,nama, "Kamu dapat menyuntik dngan suntikan mu !" );
+
+} else if (nama === "junto" && peran === "begal") {
+
+  console.log("Selamat Datang Di dunia gaib " + nama );
+  console.log("Halo",peran ,nama, "Kamu dapat membegal dngan golok mu !" );
+
+}
