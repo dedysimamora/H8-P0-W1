80c1ce833b6aa4dc23ad9b8efdb4338419a9126b
[dedysimamora] <[dedysimamora627@gmail.com]>
2019-03-14 15:19:15 +0700

    revisi ex3 4 dan sudocot

--- Exercise3-iflese-javascript.js
+++ Exercise3-iflese-javascript.js
@@ -1,25 +1,25 @@
 var nama = "junto";
-var peran = "dokter";
+var peran = "dukun";
 
 if (nama === "" && peran === ""){
 
   console.log("Nama harurs di isi!!");
 
-} else if (nama === "junto" && peran === "") {
+} else if (nama !="" && peran === "") {
 
   console.log("Halo",nama, "Peran harus di isi");
 
-} else if (nama === "junto" && peran === "dukun") {
+} else if (nama !="" && peran === "dukun") {
 
   console.log("Selamat Datang Di dunia gaib " + nama );
   console.log("Halo",peran ,nama, "Kamu dapat menyantet dengan matramu !" );
 
-} else if (nama === "junto" && peran === "dokter") {
+} else if (nama !="" && peran === "dokter") {
 
   console.log("Selamat Datang Di dunia gaib " + nama );
   console.log("Halo",peran ,nama, "Kamu dapat menyuntik dngan suntikan mu !" );
 
-} else if (nama === "junto" && peran === "begal") {
+} else if (nama !="" && peran === "begal") {
 
   console.log("Selamat Datang Di dunia gaib " + nama );
   console.log("Halo",peran ,nama, "Kamu dapat membegal dngan golok mu !" );
--- Exercise4-switch-case.js
+++ Exercise4-switch-case.js
@@ -1,12 +1,24 @@
-var tanggal = 21;
-var bulan = 1;
+var tanggal = 1;
+var bulan = 5;
 var tahun = 1945;
 
-switch(bulan){
+if (tanggal<=0 || tanggal>31){
+    console.log("Tanggal Tidak Valid");
+}
+if (tanggal>29 && bulan==2){
+  console.log("Tanggal Tidak Valid");
+}
+
+else if (bulan <=0 || bulan > 12){
+
+  console.log("Bulan Tidak Valid");
+}
+else {
+  switch(bulan){
     case 1:
-      bulan = "Januari";
-      console.log(tanggal +" "+ bulan +" "+ tahun);
-      break;
+    bulan = "Januari";
+    console.log(tanggal +" "+ bulan +" "+ tahun);
+    break;
 
     case 2:
     bulan = "Februari";
@@ -63,4 +75,87 @@ switch(bulan){
     console.log(tanggal +" "+ bulan +" "+ tahun);
     break;
 
+  }
 }
+
+
+
+
+
+// switch(bulan){
+//     case 1:
+//       bulan = "Januari";
+//       console.log(tanggal +" "+ bulan +" "+ tahun);
+//       break;
+
+//     case 2:
+//     bulan = "Februari";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 3:
+//     bulan = "Maret";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 4:
+//     bulan = "April";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 5:
+//     bulan = "Mei";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 6:
+//     bulan = "Juni";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 7:
+//     bulan = "Juli";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 8:
+//     bulan = "Agustus";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 9:
+//     bulan = "September";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 10:
+//     bulan = "Oktober";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 11:
+//     bulan = "November";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+//     case 12:
+//     bulan = "Desember";
+//     console.log(tanggal +" "+ bulan +" "+ tahun);
+//     break;
+
+
+//     case 12:
+//     console.log("tanggal yang di input tidak valid");
+//     break;
+// }
+
+
+// if(tanggal <=0 || tanggal > 31){
+//   console.log("Tanggal Tidak Valid"); 
+// }
+// else if(tahun <= 1000 || tahun >= 3000){
+//   console.log('Tahun Tidak Valid');
+// }
+// else{
+//   console.log(tanggal +" "+ bulan +" "+ tahun);
+// }
